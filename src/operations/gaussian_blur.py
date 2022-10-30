0f80ceb957fb47669b71a54636cd9fa5714406ea
dvpv <david.pava@protonmail.com>
2022-10-30 17:58:32 +0200

    fixed gblur

--- src/operations/gaussian_blur.py
+++ src/operations/gaussian_blur.py
@@ -11,13 +11,15 @@ class GaussianBlur(Operation):
     def from_dict(d: dict) -> Operation:
         return GaussianBlur(
             sigma=extract_key("sigma", d, [int]),
+            ksize=extract_key("ksize", d, [list]),
             save_result=extract_save(d),
         )
 
-    def __init__(self, sigma: int, save_result: bool = False):
+    def __init__(self, sigma: int, ksize: List[int], save_result: bool = False):
         self.__sigma = sigma
+        self.__ksize = ksize
         self.save_result = save_result
 
     def process(self, image: Image) -> Image:
-        image.image = cv2.GaussianBlur(image, sigmaX=self.__sigma)
+        image.image = cv2.GaussianBlur(image.image, self.__ksize, sigmaX=self.__sigma)
         return image
